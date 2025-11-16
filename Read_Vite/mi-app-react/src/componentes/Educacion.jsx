78ae16d74c3cc5ec6dd0161a1d00483fb782e9f4
LPAMENDEZ <panessoliliana785@gmail.com>
2025-11-16 15:34:20 -0500

    estructura inicial del componente Educacion

--- Read_Vite/mi-app-react/src/componentes/Educacion.jsx
+++ Read_Vite/mi-app-react/src/componentes/Educacion.jsx
@@ -1,1 +1,24 @@
-.
+function Educacion() {
+  return (
+    <section>
+      <h2>Educación y Formación</h2>
+
+      <ul>
+        <li>
+          <strong>Técnico en Programación de Software</strong> – SENA (2023)
+        </li>
+        <li>
+          <strong>Curso de Desarrollo Web</strong> – Platzi (2024)
+        </li>
+        <li>
+          <strong>Curso de JavaScript Básico</strong> – Udemy (2024)
+        </li>
+        <li>
+          <strong>Taller de CSS Moderno</strong> – FreeCodeCamp (2024)
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Educacion;
