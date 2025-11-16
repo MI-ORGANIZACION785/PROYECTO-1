c6ddf6a810f97b59482f793e4bca66cb8c0e8030
LPAMENDEZ <panessoliliana785@gmail.com>
2025-11-16 15:27:18 -0500

    feach: estructura inicial del perfil

--- Read_Vite/mi-app-react/src/componentes/Perfil.jsx
+++ Read_Vite/mi-app-react/src/componentes/Perfil.jsx
@@ -1,1 +1,21 @@
-. 
+function Perfil() {
+    return (
+        <section>
+            <h2>Perfil Profesional</h2>
+            <p>
+                hola mundo soy una programadora profesional 
+                de toda la vida con 20 años de experiencia.
+            </p>
+
+            <ul>
+                <li>Trabajo en equipo</li>
+                <li>Responsabilidad</li>
+                <li>Comunicacion</li>
+                <li>Aprendizaje Rapido</li>
+            </ul>
+        </section>
+    );
+    
+}
+
+
