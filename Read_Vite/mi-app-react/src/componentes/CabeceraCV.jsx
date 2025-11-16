c32c776c9f3b110017530658693b6222c145be56
LPAMENDEZ <panessoliliana785@gmail.com>
2025-11-16 15:10:30 -0500

    feach: estructura inicial de la cabecera

--- Read_Vite/mi-app-react/src/componentes/CabeceraCV.jsx
+++ Read_Vite/mi-app-react/src/componentes/CabeceraCV.jsx
@@ -1,1 +1,22 @@
-. 
+
+function CabeceraCV() {
+    const data = {
+        nombre: "Liliana Sofia Panesso Mendez",
+        rol: "Desarrolladora Front-End Jr.",
+        telefono: "3007649182",
+        corre: "panessoliliana785@gmail.com",
+        ciudad: "Medellin, Colombia"
+    };
+    
+    return(
+        <header> 
+            <h1>{data.nombre}</h1>
+            <h2>{data.rol}</h2>
+
+            <p>{data.telefono}</p>
+            <p>{data.correo}</p>
+            <p>{data.ciudad}</p>
+        </header>
+    );
+}
+export default CabeceraCV
