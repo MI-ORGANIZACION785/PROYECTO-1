8ab799a4f479ee465e2fb235eddaebbbd790e67e
LPAMENDEZ <panessoliliana785@gmail.com>
2025-11-16 15:31:52 -0500

    estructura inicial de la experiencia

--- Read_Vite/mi-app-react/src/componentes/Experiencia.jsx
+++ Read_Vite/mi-app-react/src/componentes/Experiencia.jsx
@@ -1,1 +1,34 @@
-. 
+function Experiencia() {
+  return (
+    <section>
+      <h2>Experiencia</h2>
+
+      <div>
+        <h3>Desarrolladora Front-End Jr. – TechSoft</h3>
+        <p>2023</p>
+        <p>Mantenimiento y creación de interfaces web básicas.</p>
+      </div>
+
+      <div>
+        <h3>Proyecto Personal – Sitio web de portafolio</h3>
+        <p>2024</p>
+        <p>Creación de una página personal utilizando HTML, CSS y JavaScript.</p>
+      </div>
+
+      <div>
+        <h3>Prácticas – Diseño de interfaces</h3>
+        <p>2022</p>
+        <p>Maquetación de prototipos en Figma.</p>
+      </div>
+
+      <div>
+        <h3>Proyecto Académico – App CRUD React</h3>
+        <p>2024</p>
+        <p>Aplicación CRUD con componentes y manejo de props.</p>
+      </div>
+    </section>
+  );
+}
+
+export default Experiencia;
+
